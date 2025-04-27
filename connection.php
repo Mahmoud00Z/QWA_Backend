2cfa06f8f94ae93f66fa2bfc2a5c01aca58526f6
Mahmoud00Z <zahabimahmoud@gmail.com>
2025-04-27 18:33:47 +0300

    Set up connection to MYSQL db

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername = "localhost";
+$username = "hammouda"; 
+$password = "3eb"; 
+$dbname = "quiz_app"; 
+
+$connection = new mysqli($servername, $username, $password, $dbname);
+if ($connection ->connect_error){ die("Connection failed: " . $connection->connect_error); }
+// not a real connection 3amo
